13cd3efff4c9c2791c8f9884b4a38f796f3726e5
MylesMor <mylesmor@gmail.com>
2025-07-28 11:19:38 +0100

    Add code and tests

---
+++ address_details.py
@@ -0,0 +1,74 @@
+from web3 import Web3
+import requests
+import pandas as pd
+import os
+import sys
+
+
+def retrieve_address_details(address: str) -> object:
+
+    # Ethereum RPC URL retrieved from https://chainlist.org
+    RPC_URL = "https://eth.llamarpc.com"
+
+    # Price data provided by CoinGecko
+    COINGECKO_URL = "https://api.coingecko.com/api/v3/simple/price?vs_currencies=usd&ids=ethereum"
+
+    w3 = Web3(Web3.HTTPProvider(RPC_URL))
+    if (not w3.is_connected):
+        raise Exception("Unable to connect to Ethereum chain, please try again or change the RPC URL")
+    
+    try:
+        checksum_address: str = Web3.to_checksum_address(address)
+    except ValueError:
+        raise ValueError("Invalid address hash, please check it and try again")
+    
+
+    try:
+        balance = w3.eth.get_balance(checksum_address)
+        nonce = w3.eth.get_transaction_count(checksum_address)
+        code = w3.eth.get_code(checksum_address)
+    except:
+        raise Exception("Failed to retrieve stats from RPC, please try again or change RPC URL")
+    
+    try:
+        price_usd: float = requests.get(COINGECKO_URL).json()['ethereum']['usd']
+    except Exception as e:
+        price_usd = None
+        print("Failed to retrieve price data for ethereum, continuing...")
+
+    eth_balance =  Web3.from_wei(balance, 'ether')
+
+    is_smart_contract = False
+    if code != b'0x' and code != b'':
+        is_smart_contract = True
+
+    details = {
+        "address": checksum_address,
+        "balance_eth": eth_balance,
+        "balance_wei": balance,
+        "balance_usd": price_usd if price_usd == None else float(eth_balance) * float(price_usd),
+        "transaction_count": nonce,
+        "is_smart_contract": is_smart_contract
+    }
+
+    return details
+
+
+def save_to_csv(address_details: object) -> None:
+    os.makedirs('data', exist_ok=True)
+
+    df = pd.DataFrame([address_details])
+    df.to_csv(f"./data/{address_details['address']}_details.csv", index=False)
+
+
+
+if __name__ == '__main__':
+    if len(sys.argv[1:]) != 1:
+        raise Exception("Invalid number of arguments, there should be one argument being the crypto address hash")
+    
+    address_details = retrieve_address_details(sys.argv[1])
+    save_to_csv(address_details)
+
+
+
+
---
+++ tests/test_address_details.py
@@ -0,0 +1,55 @@
+import pytest
+import sys
+import pandas as pd
+import pandas.testing as pdt
+import os
+
+# Add the parent folder to sys.path so python can find the module
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+if parent_dir not in sys.path:
+    sys.path.insert(0, parent_dir)
+
+from address_details import retrieve_address_details, save_to_csv
+
+def test_address_details():
+    # Invalid address
+    with pytest.raises(ValueError):
+        retrieve_address_details("0x47z")
+    
+    # Test with smart contract
+    contract = retrieve_address_details("0xdac17f958d2ee523a2206206994597c13d831ec7")
+    assert contract['is_smart_contract'] == True
+
+    # Test with standard address
+    address = retrieve_address_details("0x1BBc3C2E674C26264C08Bbd8a7911C404d4E3aFB")
+    print(address)
+    assert address['is_smart_contract'] == False
+
+
+def test_save_to_csv():
+
+    # Check file is saved
+    address = "0x1BBc3C2E674C26264C08Bbd8a7911C404d4E3aFB"
+    address_det = retrieve_address_details(address)
+    save_to_csv(address_det)
+
+    final_address = address_det['address']
+    file_name = f'./data/{final_address}_details.csv'
+    assert os.path.isfile(file_name) == True
+
+    # Check file matches what was retrieved
+    df = pd.read_csv(file_name)
+    expected_df = pd.DataFrame([address_det])
+
+    df['balance_eth'] = df['balance_eth'].astype(float)
+    expected_df['balance_eth'] = expected_df['balance_eth'].astype(float)
+
+    df['balance_eth'] = df['balance_usd'].astype(float)
+    expected_df['balance_eth'] = expected_df['balance_usd'].astype(float)
+    
+    pdt.assert_frame_equal(df, expected_df)
+    
+
+
+
+
